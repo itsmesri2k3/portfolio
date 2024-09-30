9082b6f3b830c9755f753509884c51cb640aaa27
itsmesri2k3 <sriraamprabu390@gmail.com>
2024-09-30 13:47:48 +0530

    v.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,22 +20,22 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// //For email sending 
-// function sendEmail(){
-//     Email.send({
-//         Host : "smtp.gmail.com",
-//         Username : "itsmesri2k3@gmail.com",
-//         Password : "SRIRAAMPRABU2003",
-//         To : 'sriraamprabu390@gmail.com',
-//         From : document.getElementById("email").value,
-//         Subject : "Message from Portfolio",
-//         Body : "Name: "+document.getElementById("name").value+
-//         "<br>Email: "+document.getElementById("email").value+
-//         "<br>Message: "+document.getElementById("msg").value
-//     }).then(
-//       message => alert("Message Send Successfully")
-//     );
-// }
+//For email sending 
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "itsmesri2k3@gmail.com",
+        Password : "SRIRAAMPRABU2003",
+        To : 'sriraamprabu390@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "Message from Portfolio",
+        Body : "Name: "+document.getElementById("name").value+
+        "<br>Email: "+document.getElementById("email").value+
+        "<br>Message: "+document.getElementById("msg").value
+    }).then(
+      message => alert("Message Send Successfully")
+    );
+}
 
 function show() {
     const section = document.getElementById('pop-up');
@@ -47,97 +47,97 @@ function show() {
     document.body.style.opacity = '1';
 };
 
-// var home = document.getElementById("home");
-// var resume = document.getElementById("resume");
-// var project = document.getElementById("project");
-// var contact = document.getElementById("contact");
+var home = document.getElementById("home");
+var resume = document.getElementById("resume");
+var project = document.getElementById("project");
+var contact = document.getElementById("contact");
 
-// function showSection(section) {
-//     // Hide all sections
-//     home.style.display = "none";
-//     resume.style.display = "none";
-//     project.style.display = "none";
-//     contact.style.display = "none";
+function showSection(section) {
+    // Hide all sections
+    home.style.display = "none";
+    resume.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
 
-//     one.style.color="rgb(161, 161, 161)";
-//     two.style.color="rgb(161, 161, 161)";
-//     three.style.color="rgb(161, 161, 161)";
-//     four.style.color="rgb(161, 161, 161)";
-//     five.style.color="rgb(161, 161, 161)";
-//     six.style.color="rgb(161, 161, 161)";
-//     seven.style.color="rgb(161, 161, 161)";
-//     eight.style.color="rgb(161, 161, 161)";
-//     // Show the selected section
-//     section.style.display = "block";
-// }
+    one.style.color="rgb(161, 161, 161)";
+    two.style.color="rgb(161, 161, 161)";
+    three.style.color="rgb(161, 161, 161)";
+    four.style.color="rgb(161, 161, 161)";
+    myLink1.style.color="rgb(161, 161, 161)";
+    myLink2.style.color="rgb(161, 161, 161)";
+    myLink3.style.color="rgb(161, 161, 161)";
+    myLink4.style.color="rgb(161, 161, 161)";
+    // Show the selected section
+    section.style.display = "block";
+}
 
-// function part1() {
-//     showSection(home);
-//     var textElement = document.getElementById('one');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent';        
-// }
-// function part2() {
-//     showSection(resume);
-//     var textElement = document.getElementById('two');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part1() {
+    showSection(home);
+    var textElement = document.getElementById('one');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent';        
+}
+function part2() {
+    showSection(resume);
+    var textElement = document.getElementById('two');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part3() {
-//     showSection(project);
-//     var textElement = document.getElementById('three');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part3() {
+    showSection(project);
+    var textElement = document.getElementById('three');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part4() {
-//     showSection(contact);
-//     var textElement = document.getElementById('four');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part4() {
+    showSection(contact);
+    var textElement = document.getElementById('four');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part5() {
-//     showSection(resume);
-//     var textElement = document.getElementById('five');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part5() {
+    showSection(home);
+    var textElement = document.getElementById('myLink1');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part6() {
-//     showSection(resume);
-//     var textElement = document.getElementById('six');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part6() {
+    showSection(resume);
+    var textElement = document.getElementById('myLink2');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part7() {
-//     showSection(resume);
-//     var textElement = document.getElementById('seven');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part7() {
+    showSection(project);
+    var textElement = document.getElementById('myLink3');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
 
-// function part8() {
-//     showSection(resume);
-//     var textElement = document.getElementById('eight');
-//     textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
-//     textElement.style.webkitBackgroundClip = 'text';  
-//     textElement.style.backgroundClip = 'text';       
-//     textElement.style.color = 'transparent'; 
-// }
+function part8() {
+    showSection(contact);
+    var textElement = document.getElementById('myLink4');
+    textElement.style.background = 'linear-gradient(to right, #F3BB61, #F2DF7B)';
+    textElement.style.webkitBackgroundClip = 'text';  
+    textElement.style.backgroundClip = 'text';       
+    textElement.style.color = 'transparent'; 
+}
